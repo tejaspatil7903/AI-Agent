115db34ae06560cf16f696094863fd500f82d082
Tejas Patil <tejasvpatil03@gmail.com>
2025-01-04 21:59:43 +0530

    fix

--- frontend/src/screens/Project.jsx
+++ frontend/src/screens/Project.jsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect, useContext, useRef } from 'react'
 import { UserContext } from '../context/user.context'
 import { useNavigate, useLocation } from 'react-router-dom'
 import axios from '../config/axios'
-import { initializeSocket, receiveMessage, sendMessage } from '../config/socket'
+import { initializeSocket, receiveMessage, sendMessage } from '../config/socket.js'
 import Markdown from 'markdown-to-jsx'
 import hljs from 'highlight.js';
-import { getWebContainer } from '../config/webcontainer'
+import { getWebContainer } from '../config/webcontainer.js'
 
 
 function SyntaxHighlightedCode(props) {
